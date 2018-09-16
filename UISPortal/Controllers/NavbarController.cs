9fa7d832f415e5cf96dcc6b512589c0381e66bbf
Haneef Puttur <cdsputtur@gmail.com>
2018-09-16 10:07:47 +0400

    Initial commit

---
+++ UISPortal/Controllers/NavbarController.cs
@@ -0,0 +1,20 @@
+﻿using UISPortal.Domain;
+using UISPortal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UISPortal.Controllers
+{
+    public class NavbarController : Controller
+    {
+        // GET: Navbar
+        public ActionResult Index()
+        {
+            var data = new Data();
+            return PartialView("_Navbar", data.navbarItems().ToList());
+        }
+    }
+}
---
+++ UISPortal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UISPortal.MvcApplication" Language="C#" %>
